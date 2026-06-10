325aa4d3b073b2f2b675bdb6e5e37a877fa72fe6
Mykhailo Shkurupii <smysyv@gmail.com>
2026-06-10 10:25:00 +0200

    feat(admin): add sidebar navigation layout

--- src/gui/views/admin_view.py
+++ src/gui/views/admin_view.py
@@ -14,3 +14,40 @@ TEXT_PRIMARY = "#FFFFFF"
 TEXT_MUTED = "#8E9297"
 
 class AdminWindow(ctk.CTkFrame):
+    def __init__(self, master, on_logout=None):
+        super().__init__(master, fg_color=BG_MAIN)
+        self.on_logout = on_logout
+        
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
+        # Fetch tournaments list from database
+        # TODO: SELECT * FROM tournaments
+        self.tournaments = []
+        
+        # Fetch teams list from database
+        # TODO: SELECT * FROM teams
+        self.teams = []
+
+        # Fetch matches list from database
+        # TODO: SELECT * FROM matches
+        self.matches = []
+        
+        # Fetch recent activity log from database
+        # TODO: SELECT * FROM activities ORDER BY created_at DESC
+        self.activities = []
+        
+        self.selected_tournament_id = 1
+
+        self.sidebar_frame = None
+        self.sidebar_buttons = {}
+        self.create_sidebar()
+
+        self.content_frame = ctk.CTkFrame(self, fg_color=BG_MAIN, corner_radius=0)
+        self.content_frame.grid(row=0, column=1, sticky="nsew", padx=20, pady=20)
+        self.content_frame.grid_rowconfigure(0, weight=1)
+        self.content_frame.grid_columnconfigure(0, weight=1)
+
+        self.current_tab = None
+        self.select_tab("Dashboard")
+
